d77e2c5c228815c81e3d396a87f58b4dbe173acf
Vy Dao <vynguyendao@gmail.com>
2021-09-22 23:23:52 +0700

    using GORM to interact with DB

---
+++ exp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "vydao"
+	dbname = "snippetbox"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.17
 require (
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.10.3 // indirect
 )
