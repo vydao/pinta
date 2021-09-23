fa7e527c6f6034e7cd829281e89fa18dc6557576
Vy Dao <vynguyendao@gmail.com>
2021-09-23 23:44:56 +0700

    Add first GORM model

--- exp/main.go
+++ exp/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -10,9 +13,15 @@ import (
 const (
 	host   = "localhost"
 	port   = 5432
-	user   = "vydao"
-	dbname = "snippetbox"
+	user   = "postgres"
+	dbname = "pinta_dev"
 )
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
@@ -21,5 +30,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Connected!")
 	defer db.Close()
+	db.LogMode(true)
+
+	db.AutoMigrate(&User{})
+
+	name, email := getInfo()
+	u := &User{
+		Name:  name,
+		Email: email,
+	}
+	if err = db.Create(u).Error; err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", u)
+}
+
+func getInfo() (name, email string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	email = strings.TrimSpace(email)
+	return name, email
 }
