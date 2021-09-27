38ed82e9e08e34d8e0d47c4d8c793cae6c526de9
Vy Dao <vynguyendao@gmail.com>
2021-09-27 23:06:05 +0700

    Refactoring model User

--- models/users.go
+++ models/users.go
@@ -32,19 +32,27 @@ func (us *UserService) Close() error {
 
 func (us *UserService) ByID(id uint) (*User, error)  {
 	var user User
-	err := us.db.Where("id = ?", id).First(&user).Error
-
-	switch err {
-	case nil:
-		return &user, nil
-	case gorm.ErrRecordNotFound:
-		return nil, ErrNotFound
-	default:
+	db := us.db.Where("id = ?", id)
+	err := first(db, &user)
+	if err != nil {
 		return nil, err
 	}
+	return &user, nil
 }
 
 func (us *UserService) DestructiveReset()  {
 	us.db.DropTableIfExists(&User{})
 	us.db.AutoMigrate(&User{})
+}
+
+func (us *UserService) Create(user *User) error {
+	return us.db.Create(user).Error
+}
+
+func first(db *gorm.DB, dst interface{}) error  {
+	err := db.First(dst).Error
+	if err == gorm.ErrRecordNotFound {
+		return ErrNotFound
+	}
+	return err
 }
